0a282c14b2df0c2a4a7120ad967b3710a5aad51f
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-09-29 10:27:28 -0300

    Se agregaron archivos de rutas y controladores

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const cardRouter = require('/routes/cardRouter')
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/card', cardRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
